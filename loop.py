94ae9c984f8e512052e00df19d98cca75adefa57
kaitlynn <79729528+kaitlynn333@users.noreply.github.com>
2021-10-08 13:30:31 -0700

    Create file to display basic python code

---
+++ loop.py
@@ -0,0 +1,29 @@
+# number, strings, lists, booleans, conditionals, loops
+
+# number
+a = 5
+b = 7
+sum = a + b
+
+# strings
+saying_hi = "hi my name is kaitlynn"
+print(saying_hi)
+
+# lists
+age_list = [5, 7, 9, 11]
+print(age_list[2])
+
+# booleans
+is_it_raining = False
+print( not is_it_raining) # true
+
+# conditionals
+if is_it_raining:
+    print("It is raining!")
+else:
+    print("It is NOT raining")
+
+# loops
+for i in range(5):
+    print(stri(i) + ": hi")
+
